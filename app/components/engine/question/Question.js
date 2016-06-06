3293d47a63b58b8cbb6d729ecd1f6f18b33c3c1d
Swapnil Bawkar <swapnil.bawkar@ingramcontent.com>
2016-06-06 14:53:42 +0530

    Question view changes

--- app/components/engine/question/Question.js
+++ app/components/engine/question/Question.js
@@ -8,14 +8,20 @@ var Question = React.createClass({
     propTypes: {
         question: React.PropTypes.object
     },
+    getInitialState: function() {
+        return {
+            btnText: 'CHECK',
+            currentQuestion: {},
+            checkBtnClicked: false
+        };
+    },
     render: function() {
         var options = '';
         if(this.props.question.answers) {
             options = this.props.question.answers.answer.map(function (option, index) {
-                return <li key={index}>{option}</li>;
-            });
+                return <li key={index} onClick={this.onAnswerClick.bind(this, option)}><p>{option}</p></li>;
+            }.bind(this));
         }
-        
         return (
             <div className="question">
                 <div className="image-wrapper">
@@ -25,15 +31,37 @@ var Question = React.createClass({
                     <p>{this.props.question.text}</p>
                 </div>
                 <div className="question-options">
-                    <ul>
+                    <ul className="option-ul">
                         {options}
                     </ul>
                 </div>
+                <div className="feedback">
+                    <ul>
+                        <li>{this.state.userAnswer}</li>
+                    </ul>
+                </div>
                 <div className="footer-btn">
-                    <button type="button" onClick={this.handleClick}>NEXT</button>
+                    <button type="button" onClick={this.handleClick}>{this.state.btnText}</button>
                 </div>
             </div>
        );
+    },
+    handleClick: function(event) {
+        this.setState({
+            btnText: 'NEXT',
+            checkBtnClicked: true,
+            userAnswer: this.props.question.userAnswer
+        });
+    },
+    onAnswerClick: function (option) {
+        var index = this.props.question.answers.answer.indexOf(option);
+        if(index === 0) {
+            this.props.question.correct = true;
+        } else {
+            this.props.question.correct = false;
+        }
+        this.props.question.userAnswer = option;
+        this.answerOption.style.backgroundColor = '#FFC400';
     }
 });
 
