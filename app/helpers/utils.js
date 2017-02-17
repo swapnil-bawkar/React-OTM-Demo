948ca1e637f7a816858937d6d6b668e7ec306cdf
Swapnil Bawkar <swapnil.bawkar@ingramcontent.com>
2017-02-17 16:35:58 +0530

    Change xml url

--- app/helpers/utils.js
+++ app/helpers/utils.js
@@ -5,7 +5,7 @@ var axios = require('axios');
 var X2JS = require("x2js");
 
 function loadData() {
-    return axios.get('/activities/match_choose_otm/SC3eOTM_00_01_04.xml')
+    return axios.get('./activities/match_choose_otm/SC3eOTM_00_01_04.xml')
         .then(function(response) {
             var x2js = new X2JS();
             var document = x2js.xml2js(response.data);
